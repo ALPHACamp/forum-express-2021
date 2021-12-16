f39f00de8e349f477e7a4eb473f3cc04958768b0
Carrot7712 <carrot7712@gmail.com>
2021-12-16 18:56:54 +0800

    R01 通過

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -14,7 +14,7 @@ router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 
-router.patch('/users/:id', adminController.patchUsers)
+router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
 
 router.get('/categories/:id', categoryController.getCategories)
